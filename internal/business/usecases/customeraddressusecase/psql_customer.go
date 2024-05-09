c5f270c825b1c8c18884a10aa0e410362d4a99f8
erlend <erlenrom@stud.ntnu.no>
2024-05-09 11:32:28 +0200

    Modified sql-queries

--- internal/business/usecases/customeraddressusecase/psql_customer.go
+++ internal/business/usecases/customeraddressusecase/psql_customer.go
@@ -2,6 +2,7 @@ package customeraddressusecase
 
 import (
 	"DB_Project/internal/business/domains/customeraddressdomain"
+	"context"
 	"database/sql"
 )
 
@@ -15,18 +16,72 @@ func NewPSQLCustomer(db *sql.DB) customeraddressdomain.CustomerDomain {
 	}
 }
 
-func (psql *PSQLCustomer) CreateCustomer(customer *customeraddressdomain.CreateCustomer) (*customeraddressdomain.Customer, error) {
+func (psql *PSQLCustomer) CreateCustomer(ctx context.Context, customer *customeraddressdomain.CreateCustomer) (*customeraddressdomain.Customer, error) {
+	tx, err := psql.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = tx.ExecContext(ctx, `INSERT INTO "customer" (username, password, first_name, last_name, email, phone_number) VALUES ($1, $2, $3, $4, $5, $6)`,
+		customer.Username, customer.Password, customer.FirstName, customer.LastName, customer.Email, customer.PhoneNumber); err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
-func (psql *PSQLCustomer) GetCustomerByUsername(username string) (*customeraddressdomain.Customer, error) {
+func (psql *PSQLCustomer) GetCustomerByUsername(ctx context.Context, username string) (*customeraddressdomain.Customer, error) {
+	tx, err := psql.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = tx.ExecContext(ctx, `SELECT first_name, last_name, email, phone_number FROM "customer" WHERE username = $1 AND deleted = FALSE`, username); err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
-func (psql *PSQLCustomer) UpdateCustomer(username string, customer *customeraddressdomain.CreateCustomer) (*customeraddressdomain.Customer, error) {
+func (psql *PSQLCustomer) UpdateCustomer(ctx context.Context, username string, customer *customeraddressdomain.CreateCustomer) (*customeraddressdomain.Customer, error) {
+	tx, err := psql.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err = tx.ExecContext(ctx, `UPDATE "customer" SET first_name = $1, last_name = $2, email = $3, phone_number = $4 WHERE username = $5`,
+		customer.FirstName, customer.LastName, customer.Email, customer.PhoneNumber, username); err != nil {
+		tx.Rollback()
+		return nil, err
+	}
+
+	if err = tx.Commit(); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
 
-func (psql *PSQLCustomer) SoftDeleteCustomer(username string) error {
+func (psql *PSQLCustomer) SoftDeleteCustomer(ctx context.Context, username string) error {
+	tx, err := psql.DB.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	if _, err = tx.ExecContext(ctx, `UPDATE "customer" SET deleted = true WHERE username = $1`, username); err != nil {
+		tx.Rollback()
+		return err
+	}
+
 	return nil
 }
